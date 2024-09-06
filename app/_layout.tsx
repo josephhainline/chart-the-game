6aa37be3cfb449c38e8b3e706b7a0047d1208256
Joe Hainline <joseph.hainline@gmail.com>
2024-09-05 20:54:42 -0500

    Update route name for "(tabs)" to "(tabs)/game-tracker" in RootLayoutNav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -41,7 +41,7 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)/game-tracker" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
